aebfa09745142d836deca5a0981ac62dc1679ae3
kruthagna-j <jujigirikruthaghna@gmail.com>
2026-09-09 19:02:34 +0530

    fix(android): expose alert dialog alias

--- android/app/src/main/java/com/spotifusion/app/ModernCompat.kt
+++ android/app/src/main/java/com/spotifusion/app/ModernCompat.kt
@@ -5,8 +5,8 @@ import android.widget.EditText
 import android.text.Editable
 import android.text.TextWatcher
 
-@Suppress("unused")
 typealias ModernAlertDialog = AlertDialog
+typealias AlertDialogCompat = AlertDialog
 
 fun EditText.addTextChangedListener(callback: (Editable?) -> Unit) {
     addTextChangedListener(object : TextWatcher {
